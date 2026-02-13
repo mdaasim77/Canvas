e945d17ca23d841cc352ac880556835dc48e719a
mdaasim77 <khanaasim9910@gmail.com>
2026-02-13 15:42:27 +0530

    grADIENT

--- script.js
+++ script.js
@@ -22,34 +22,42 @@ let context = canvas.getContext("2d");
 
 // ============= FOR LOOP CODE FOR CANVAS.
 
-let x = 100;
-let y = 100;
-let w = 80;
-let h = 80;
-let g = w + 10;
+// let x = 100;
+// let y = 100;
+// let w = 80;
+// let h = 80;
+// let g = w + 10;
 
-for (i = 0; i < 5; i++) {
-  for (j = 0; j < 5; j++) {
-    context.beginPath();
-    context.rect(x + g * i, y + g * j, w, h); // use variable instead of number is also a way
-    context.stroke();
+// for (i = 0; i < 5; i++) {
+//   for (j = 0; j < 5; j++) {
+//     context.beginPath();
+//     context.rect(x + g * i, y + g * j, w, h); // use variable instead of number is also a way
+//     context.stroke();
 
-    // new concept in this we use condition statement
+//     // new concept in this we use condition statement
 
-    // if(i>0 && i<4){
-    //     context.beginPath();
-    //     context.rect(x+10+g*i,y+10+g*j,w-20,h-20);
-    //     context.stroke();
-    // }
+//     // if(i>0 && i<4){
+//     //     context.beginPath();
+//     //     context.rect(x+10+g*i,y+10+g*j,w-20,h-20);
+//     //     context.stroke();
+//     // }
 
-    // ============= using math.randon() fn we create random square in our grid layout.
+//     // ============= using math.randon() fn we create random square in our grid layout.
 
-    if (Math.random() > 0.5) {
-      context.beginPath();
-      context.rect(x + 10 + g * i, y + 10 + g * j, w - 20, h - 20);
-      context.stroke();
-    }
-  }
-}
+//     // if (Math.random() > 0.5) {
+//     //   context.beginPath();
+//     //   context.rect(x + 10 + g * i, y + 10 + g * j, w - 20, h - 20);
+//     //   context.stroke();
+//     // }
+//   }
+// }
 
-// ============= .
+// ============= LINEAR GRADIENT COLOR.
+
+let lg = context.createLinearGradient(0, 0, 200, 0);
+
+lg.addColorStop(0, "red");
+lg.addColorStop(1, "blue");
+
+context.fillStyle = lg;
+context.fillRect(50, 50, 200, 100 );
