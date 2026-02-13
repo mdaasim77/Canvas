56fc73ba9d22220f588a340ab23542445b25a3d2
mdaasim77 <khanaasim9910@gmail.com>
2026-02-13 17:18:12 +0530

    add new canvas2 file and work start

---
+++ canvas2.js
@@ -0,0 +1,16 @@
+// ========== USE THESE TWO LINES TO INITIAL ADD CANVAS.
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+// ========== GIVE RECTANGLE BORDER LINEAR GRADIENT.
+
+var gradient = ctx.createLinearGradient(0, 0, 170, 0);
+gradient.addColorStop(0, "blue");
+gradient.addColorStop(0.5, "red");
+gradient.addColorStop(1.0, "green");
+ctx.strokeStyle = gradient;
+ctx.lineWidth=5
+ctx.strokeRect(10, 10, 200, 200);
+
+
