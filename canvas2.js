5ae0fcf703ba3a5f2d8ce8e8f158173d98aa9cf0
mdaasim77 <khanaasim9910@gmail.com>
2026-02-15 13:35:02 +0530

    work done

--- canvas2.js
+++ canvas2.js
@@ -5,12 +5,12 @@ var ctx = canvas.getContext("2d");
 
 // ========== GIVE RECTANGLE BORDER LINEAR GRADIENT.
 
-// var gradient = ctx.createLinearGradient(0, 0, 200, 0);
-// gradient.addColorStop(0, "blue");
-// gradient.addColorStop(0.5, "red");
-// gradient.addColorStop(1.0, "green");
-// ctx.strokeStyle = gradient;
-// ctx.lineWidth = 5;
-// ctx.strokeRect(10, 10, 200, 200);
+var gradient = ctx.createLinearGradient(0, 0, 200, 0);
+gradient.addColorStop(0, "blue");
+gradient.addColorStop(0.5, "red");
+gradient.addColorStop(1.0, "green");
+ctx.strokeStyle = gradient;
+ctx.lineWidth = 5;
+ctx.strokeRect(10, 10, 200, 200);
 
 // ========== GIVE RECTANGLE BORDER LINEAR GRADIENT.
